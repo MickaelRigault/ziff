8a1bbea0c5c1fd91e0d7c2db1ea5caec38390944
Romain Graziani <romain.graziani@clermont.in2p3.fr>
2020-09-28 10:08:50 +0200

    Added download_query

---
+++ ziff/scripts/download_query.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+################################################################################
+# Filename:          download_query.py
+# Description:       script description
+# Author:            Romain Graziani <romain.graziani@clermont.in2p3.fr>
+# Author:            $Author: rgraziani $
+# Created on:        $Date: 2020/09/25 16:23:01 $
+# Modified on:       2020/09/28 09:51:38
+# Copyright:         2019, Romain Graziani
+# $Id: download_query.py, 2020/09/25 16:23:01  RG $
+################################################################################
+
+"""
+.. _download_query.py:
+
+download_query.py
+==============
+
+
+"""
+__license__ = "2019, Romain Graziani"
+__docformat__ = 'reStructuredText'
+__author__ = 'Romain Graziani <romain.graziani@clermont.in2p3.fr>'
+__date__ = '2020/09/25 16:23:01'
+__adv__ = 'download_query.py'
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--query",type=str,default = "obsjd BETWEEN 2458554.5 AND 2458564")
+
+
+from ztfquery import query
+zquery = query.ZTFQuery() 
+zquery.load_metadata(sql_query = query)
+print(zquery.metatable)
+keys = ['sciimg.fits', 'mskimg.fits', 'psfcat.fits']
+for _key in keys:
+    zquery.download_data(_key,show_progress=True, notebook=False, nprocess=8)
+        
+
+# End of download_query.py ========================================================
