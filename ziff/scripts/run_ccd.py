689e46d4b0fb542812518a8d69e9a4f243b457d8
Romain Graziani <romain.graziani@clermont.in2p3.fr>
2020-09-30 10:03:23 +0200

    Multiple rows in run_cdd

--- ziff/scripts/run_ccd.py
+++ ziff/scripts/run_ccd.py
@@ -6,7 +6,7 @@
 # Author:            Romain Graziani <romain.graziani@clermont.in2p3.fr>
 # Author:            $Author: rgraziani $
 # Created on:        $Date: 2020/09/24 14:06:57 $
-# Modified on:       2020/09/29 10:01:37
+# Modified on:       2020/09/30 10:03:06
 # Copyright:         2019, Romain Graziani
 # $Id: run_ccd.py, 2020/09/24 14:06:57  RG $
 ################################################################################
@@ -36,37 +36,36 @@ import pkg_resources
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--filename",type=str)
-parser.add_argument("--row",type=int)
+parser.add_argument("--rows",type=int, nargs='+')
 parser.add_argument("--make_cats",type=int,default=1)
 parser.add_argument("--run",type=int,default=1)
 parser.add_argument("--shapes",type=int,default=1)
 
 
 args = parser.parse_args()
+rows = np.atleast_1d(args.rows)
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-if args.make_cats:
-    z = ziff.ziff.Ziff.from_file(args.filename, row = args.row, build_default_cat = True, load_default_cat = False, save_cat = True)
-    #z.save_all_cats(overwrite = True)
 
-else:
-    z = ziff.ziff.Ziff.from_file(args.filename, row = args.row, build_default_cat = False, load_default_cat = True, save_cat = False)
-z.set_config_value('psf,interp,order',5)
-z.set_config_value('psf,outliers,max_remove',20)
-#map_file = pkg_resources.resource_filename('ziff', 'data/interpolator.pkl')
-#z.set_config_value('psf,interp,interpolation_map_file',map_file)
-#z.set_config_value('psf,interp,type','BasisPolynomialPlusMap')
-
-if args.run:
-    z.run_piff('gaia_calibration',overwrite_cat=True)
-    
-if args.shapes:
-    z.set_config_value('i/o,nstars', 2000) 
-    stars = z.make_stars('gaia_full',overwrite_cat=True, append_df_keys = ['RPmag','BPmag','colormag'])
-    new_stars = z.reflux_stars(stars, fit_center = True, use_minuit = True)
-    res = z.compute_residuals(new_stars)
-    shapes = z.compute_shapes(new_stars,save=True)
+for row in rows:
+    if args.make_cats:
+        z = ziff.ziff.Ziff.from_file(args.filename, row = args.row, build_default_cat = True, load_default_cat = False, save_cat = True)
+    else:
+        z = ziff.ziff.Ziff.from_file(args.filename, row = args.row, build_default_cat = False, load_default_cat = True, save_cat = False)
+    z.set_config_value('psf,interp,order',5)
+    z.set_config_value('psf,outliers,max_remove',20)
+    #map_file = pkg_resources.resource_filename('ziff', 'data/interpolator.pkl')
+    #z.set_config_value('psf,interp,interpolation_map_file',map_file)
+    #z.set_config_value('psf,interp,type','BasisPolynomialPlusMap')
+    if args.run:
+        z.run_piff('gaia_calibration',overwrite_cat=True)    
+    if args.shapes:
+        z.set_config_value('i/o,nstars', 2000) 
+        stars = z.make_stars('gaia_full',overwrite_cat=True, append_df_keys = ['RPmag','BPmag','colormag'])
+        new_stars = z.reflux_stars(stars, fit_center = True, use_minuit = True)
+        res = z.compute_residuals(new_stars)
+        shapes = z.compute_shapes(new_stars,save=True)
 
 
 
