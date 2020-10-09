d1884a319d1306218ce83f3b7a325aea196f840c
Romain Graziani <romain.graziani@clermont.in2p3.fr>
2020-10-09 11:29:31 +0200

    Added pre and post creation in download_target

--- ziff/scripts/download_target.py
+++ ziff/scripts/download_target.py
@@ -6,7 +6,7 @@
 # Author:            Romain Graziani <romain.graziani@clermont.in2p3.fr>
 # Author:            $Author: rgraziani $
 # Created on:        $Date: 2020/09/25 16:23:01 $
-# Modified on:       2020/10/09 10:41:41
+# Modified on:       2020/10/09 11:28:22
 # Copyright:         2019, Romain Graziani
 # $Id: download_query.py, 2020/09/25 16:23:01  RG $
 ################################################################################
@@ -31,6 +31,9 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--target",type=str,default = "ZTF19aanbojt")
 parser.add_argument("--overwrite",type=int,default = 1)
 parser.add_argument("--nprocess",type=int,default = 1)
+parser.add_argument("--priorcreation",type=int,default = 300)
+parser.add_argument("--postlast",type=int,default = 300)
+
 
 
 args = parser.parse_args()
@@ -40,7 +43,7 @@ from ztfquery import query
 
 m = marshal.MarshalAccess.load_local()
 zquery = query.ZTFQuery()
-zquery.load_metadata(kind = 'sci', **m.get_target_metadataquery(args.target))
+zquery.load_metadata(kind = 'sci', **m.get_target_metadataquery(args.target, priorcreation = args.priorcreation, postlast = args.postlast))
 print(zquery.metatable)
 keys = ['sciimg.fits', 'mskimg.fits', 'psfcat.fits']
 for _key in keys:
