7238b3cf6854045b14f44fb537b6db25e44742f9
MickaelRigault <mick.rigault@gmail.com>
2020-12-18 16:36:21 +0100

    new strucutre

---
+++ ziff/utils.py
@@ -0,0 +1,33 @@
+
+
+
+
+""" Generic tools """
+import numpy as np
+
+def is_running_from_notebook():
+    """ Test if currently ran in notebook """
+    return running_from() == "notebook"
+
+def running_from():
+    """  Where is the code running from ?"""
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == 'ZMQInteractiveShell':
+            return "notebook"   # Jupyter notebook or qtconsole
+        elif shell == 'TerminalInteractiveShell':
+            return "terminal"  # Terminal running IPython
+        else:
+            return "other"  # Other type (?)
+    except NameError:
+        return None 
+
+def avoid_duplicate(string_array):
+    """ """
+    string_array = np.asarray(string_array, dtype=object)
+    occurence = {k:v for k,v in zip(*np.unique(string_array, return_counts=True))}
+    for name_,nname_ in occurence.items():
+        if nname_>1:
+            string_array[np.in1d(string_array,name_)] = [name_+f"{i+1}" for i in range(nname_)]
+            
+    return np.asarray(string_array, dtype="str")
