234f87d5c3b30b9e1c72b7f9fb462578661a0249
MickaelRigault <mick.rigault@gmail.com>
2021-01-08 17:16:43 +0100

    adding CCIN2P3 file

--- bin/qsub_ziffit.sh
+++ bin/qsub_ziffit.sh
@@ -3,11 +3,24 @@
 #####################################
 # exemple job script pour options GE
 #####################################
+#$ -o $HOME/jobs/
+#$ -M m.rigault@ipnl.in2p3.fr
+#$ -m be   ## envoie un email quand le job commence et termine
+# Mandatory: group
+#$ -P P_ztf
+#
+# Must Access SPS for storage
+#$ -l sps=1
+#
+# The jobs take a lot of memory and fail on long
+#$ -q long
+#
+#$ -l h_rss=2G
 
 # RUNNING THE CODE
 
 for i in $(cat $1); do
-    qsub /sps/lsst/users/rigault/anaconda3/bin/ziffit.py ${1} --catalog 'ps1cat' --addfilter 'gmag' '14' '18'  -o $HOME/jobs/ -M m.rigault@ipnl.in2p3.fr -m be -P P_ztf -l sps=1 -q long -l h_rss=2G
+    qsub ziffit.py ${1} --catalog 'ps1cat' --addfilter 'gmag' '14' '18'
 done
 
 
