39e122a24628345e59c809225f8957f7b44e5148
MickaelRigault <mick.rigault@gmail.com>
2021-01-08 17:58:53 +0100

    adding CCIN2P3 file

--- bin/qsub_ziffit.sh
+++ bin/qsub_ziffit.sh
@@ -8,7 +8,7 @@
 # RUNNING THE CODE
 
 for i in $(cat $1); do
-    qsub -P P_ztf -l sps=1 -q long -l h_rss=2G -o $HOME/jobs/ziffit/ /sps/lsst/users/rigault/anaconda3/bin/ziffit.py ${i} --catalog 'ps1cal' --addfilter 'gmag' '14' '18'
+    qsub -P P_ztf -l sps=1 -q long -l h_rss=2G -o $HOME/jobs/ziffit/ -e $HOME/jobs/ziffit/ /sps/lsst/users/rigault/anaconda3/bin/ziffit.py ${i} --catalog 'ps1cal' --addfilter 'gmag' '14' '18'
 done
 
 
