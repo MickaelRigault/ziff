8d078027f5b2f18e607cd87fca9a0fe2ea0f45c7
MickaelRigault <mick.rigault@gmail.com>
2021-02-08 20:01:03 +0100

    test

--- ziff/scripts/ziffit.py
+++ ziff/scripts/ziffit.py
@@ -46,19 +46,19 @@ def ziffit_single(file_, use_dask=False, overwrite=False,
     ziff   = delayed(base.ZIFF)(sciimg, mkimg, fetch_psf=False)
     #
     # - Get the catalog    
-    catfit    = delayed(base.get_gaia_catalog)(ziff, isolationlimit=isolationlimit,
+    cat   = delayed(base.get_gaia_catalog)(ziff, isolationlimit=isolationlimit,
                                                 gmag_range=fit_gmag, shuffled=True)
     #
     # - Fit the PSF
-    psf    = delayed(base.estimate_psf)(ziff, catfit,
+    psf    = delayed(base.estimate_psf)(ziff, cat,
                                             interporder=interporder, nstars=nstars,
                                             maxoutliers=maxoutliers, verbose=False)
     #
     # - Compute shapes
-    catshp = delayed(base.get_gaia_catalog)(ziff, writeto="shape", gmag_range=shape_gmag,
-                                                shuffled=True)
+    #catshp = delayed(base.get_gaia_catalog)(ziff, writeto="shape", gmag_range=shape_gmag,
+    #                                            shuffled=True)
     # shapes
-    shapes  = delayed(base.get_shapes)(ziff, psf, catshp, store=True)
+    shapes  = delayed(base.get_shapes)(ziff, psf, cat, store=True)
     
     return shapes[["sigma_model","sigma_data"]].median(axis=0).values
     
